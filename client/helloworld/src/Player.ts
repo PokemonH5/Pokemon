fd9d742f7dd5a93f061e6b22fba165747b52a55b
builderss <q503851380@outlook.com>
2016-12-25 13:58:23 +0800

    Revert "去掉服务器注释"
    This reverts commit a7aca9e6075660a397d9f8405ed7d83f5e4d7e0f.

--- client/helloworld/src/Player.ts
+++ client/helloworld/src/Player.ts
@@ -95,7 +95,7 @@ class LocationEvent extends egret.TouchEvent
     //解决方案1：位置检测
     public _player_x: number = 0;
     public _player_y: number = 0;
-    
+
     //解决方案二：图层碰撞检测：待商榷
 
     public constructor(type:string, bubbles:boolean=false, cancelable:boolean=false)
